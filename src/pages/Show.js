3ea81444790fa259561ebdf4238a9c7d9331be32
adriancgarcia <adriancgarcia@gmail.com>
2023-11-12 23:23:16 -0600

    fixing more css bugs

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -15,7 +15,7 @@ function Show (props){
 
     return (
         <div style={div}>
-            <div className="appliedJobs">    
+            <div class="appliedJobs">    
                 <h2>{post.position}</h2>
                 <h3>{post.company_name}</h3>
                 <h3>{post.job_description}</h3>
@@ -28,10 +28,10 @@ function Show (props){
                 <h3>{post.application_origin}</h3>
                 <h3>{post.thankyou_sent}</h3>
             </div>
-            <div style ={{ textAlign: "center" }} className="updateApp">
+            <div style ={{ textAlign: "center" }} class="updateApp">
                 <h1>Update Job Application</h1>
                 <Form method="post" action= {`/update/${post.id}`}>
-                    <fieldset className="form">
+                    <fieldset class="form">
                         <label htmlFor="position">Job Position</label>
                         <input type="text" name="position" placeholder="Job Position" defaultValue={post.position}/>
                         
@@ -46,8 +46,7 @@ function Show (props){
                         <label htmlFor="type_of_resume_sent">Type of resume sent</label>
                         <input type="text" name="type_of_resume_sent" placeholder="ATS or Design Resume" defaultValue={post.type_of_resume_sent}/>
                         
-                        <label htmlFor="date_applied">Date Applied</label>
-                        <input type="date" name="date_applied" placeholder="Date Applied" defaultValue={post.date_applied}/>
+                        <h3>Date Applied</h3><input type="date" name="date_applied" placeholder="Date Applied" defaultValue={post.date_applied}/>
                         
                         <label htmlFor="hiring_manager">Hiring Manager</label>
                         <input type="text" name="hiring_manager" placeholder="Hiring Manager" defaultValue={post.hiring_manager}/>
@@ -58,7 +57,7 @@ function Show (props){
                         <label htmlFor="interview_status">Interview Status</label>
                         <input type="text" name="interview_status" placeholder="Interview Status" defaultValue={post.interview_status}/>
 
-                        <label htmlFor="application_origin">Origin of Job Posting</label>
+                        <label htmlFor="applicatoin_origin">Origin of Job Posting</label>
                         <input type="text" name="application_origin" placeholder="Where did you find application" defaultValue={post.application_origin}/>
                         
                         <h3>Did you send a thank you note?</h3><input type="checkbox" name="thankyou_sent" placeholder="Thank you note sent" defaultValue={post.thankyou_sent}/>
